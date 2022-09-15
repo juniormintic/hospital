aa419ff84f57507a74e6ca858c26e049708f95d8
junior <jhgalvanmintic@gmail.com>
2022-09-14 19:58:19 -0500

    version de junior hospital

--- sistema/settings.py
+++ sistema/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!5nr91=0yvll969u$(a=dn3#9chjtzygp=+@oj@shcfxt8shhc'
+SECRET_KEY = '!5nr91=0yvll969u$(a=dn3#9chjtzygp=+@oj@shcfxt8shhc' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
